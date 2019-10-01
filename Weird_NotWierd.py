f84d056db8562ee5a612e915ecd8f5aa4a5a11f3
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-10-01 22:09:05 +0530

    Create Weird_NotWierd.py

---
+++ Weird_NotWierd.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+n=int(input())
+if n%2!=0:
+    print('Weird')
+else:
+    if n in range(2,6):
+        print('Not Weird')
+    elif n in range(6,21):
+        print('Weird')
+    else:
+        print('Not Weird')
