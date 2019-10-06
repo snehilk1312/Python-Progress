4b84a75c02fc5c93b0c552305a91e00998e86e21
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-10-06 16:51:24 +0530

    Create closures_and_decorators.py

---
+++ closures_and_decorators.py
@@ -0,0 +1,37 @@
+'''
+Let's use decorators to build a name directory! You are given some information about N people.
+Each person has a first name, last name, age and sex. 
+Print their names in a specific format sorted by their age in ascending order
+i.e. the youngest person's name should be printed first. For two people of the same age, print them in the order of their input.
+SAMPLE INPUT:
+3
+Mike Thomson 20 M
+Robert Bustle 32 M
+Andria Bustle 30 F
+
+SAMPLE OUTPUT:
+Mr. Mike Thomson
+Ms. Andria Bustle
+Mr. Robert Bustle
+'''
+import operator
+m=[]
+def sortSecond(val): 
+    return int(val[2])  
+def person_lister(f):
+    def inner(people):
+        #print(people)
+        people.sort(key=sortSecond)
+        #print(people)
+        for i in people:
+            k=f(i)
+            m.append(k)
+        return m
+    return inner
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
