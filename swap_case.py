7b8fdc1c878dddd4d6718fd9b0b7d4f8526c1537
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-10-12 17:04:54 +0530

    Update swap_case.py

--- swap_case.py
+++ swap_case.py
@@ -1,3 +1,14 @@
+'''def swap_case(s):
+    t=[i.upper() if i.islower() else i.lower() for i in s]
+    k=''.join(t)
+    return k
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+  '''
+
+
 def swap_case(s):
     t=''
     for i in s:
