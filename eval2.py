c46395ddda0a37ef63ba7ed3561eadfeb11d340e
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-10-28 00:47:52 +0530

    Create eval2.py

---
+++ eval2.py
@@ -0,0 +1,7 @@
+'''
+You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var). 
+LINK:
+https://www.hackerrank.com/challenges/python-eval/problem
+'''
+k=input()
+eval(k)
