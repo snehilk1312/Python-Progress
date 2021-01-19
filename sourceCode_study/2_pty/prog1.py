a1d1132eee615edcf3f606f32e87c481c9260afe
snehil <44499005+snehilk1312@users.noreply.github.com>
2021-01-19 20:38:12 +0530

    pty
    https://docs.python.org/3/library/pty.html

---
+++ sourceCode_study/2_pty/prog1.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+import pty
+import sys
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', dest='append', action='store_true')
+parser.add_argument('-p', dest='use_python', action='store_true')
+parser.add_argument('filename', nargs='?', default='typescript')
+options = parser.parse_args()
+
+print(options.append)
